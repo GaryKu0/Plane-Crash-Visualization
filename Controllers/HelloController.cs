281300dc8e486accb5c1735cf73c4220da2d6148
julia <julia@DESKTOP-73OIJCN>
2025-06-08 20:23:00 +0200

    Projektdateien hinzufügen.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("Hallo von der API!");
+}
---
+++ PlaneCrashVisualizationClient/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { ref, onMounted } from 'vue'
+
+    const message = ref('Lade...')
+
+    onMounted(async () => {
+        const res = await fetch('/api/hello')
+        message.value = await res.text()
+    })
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+</template>
